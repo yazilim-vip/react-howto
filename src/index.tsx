10dc1404d510678ce18a2dc4ca316dd4594bf158
Emre Sen <maemresen@yazilim.vip>
2021-01-06 18:20:53 +0300

    showcase(wip): base project created

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import WebFont from 'webfontloader'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-sliding-pane/dist/react-sliding-pane.css'
 
 import App from './showcase/App'
-import { configureStore } from './showcase/redux/configureStore'
 
-const { store, persistor } = configureStore()
-
-ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 WebFont.load({
     google: {
